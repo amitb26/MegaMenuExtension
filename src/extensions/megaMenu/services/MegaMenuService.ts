37ab85d81809af1fe47e1d9e3bae6f4bc19077ba
Amit Sharma <87112895+amitb26@users.noreply.github.com>
2025-07-04 14:49:46 +0530

    Update MegaMenuService with comprehensive menu data

--- src/extensions/megaMenu/services/MegaMenuService.ts
+++ src/extensions/megaMenu/services/MegaMenuService.ts
@@ -227,11 +227,101 @@ export class MegaMenuService {
           megaMenu: {
             columns: [
               {
-                title: "Workspaces",
+                title: "",
                 items: [
                   { title: "Academic Affairs Staff Workspace", href: "/sites/academicaffairsworkspace" },
+                  { title: "Adjunct Faculty Workspace", href: "/sites/adjunctfacultyworkspace" },
+                  { title: "Admitted J.D. Student Portal", href: "/sites/admittedjdstudentportal" },
+                  { title: "Bar Exam Info", href: "/sites/barexaminfo" },
+                  { title: "Bar Exam Results", href: "/sites/barexamresults" },
+                  { title: "CJEC Post Grad Judicial Clerkship for J.D. Students and Alumni", href: "/sites/postgraduatejudicialclerkships" },
+                  { title: "CJEC Workspace for Administrators", href: "/sites/cjecworkspace" },
+                  { title: "Climate and Sustainability", href: "/sites/clesi" },
+                  { title: "Clinic Staff and Faculty Workspace", href: "/sites/clinicstaffandfacultyworkspace" },
+                  { title: "Clinics", href: "/sites/clinics" },
+                  { title: "CMS Workspace", href: "/sites/cms" },
+                  { title: "Course Evaluations", href: "/sites/courseevaluations" },
+                  { title: "CPC Career Services for Current J.D. Students", href: "/sites/careerservicesforcurrentjdstudents" },
+                  { title: "CPC Career Services for Current J.D. Students", href: "/sites/careerservicesforcurrentjdstudents_old" }
+                ]
+              },
+              {
+                title: "",
+                items: [
+                  { title: "CPC Career Services for J.D. Alumni", href: "/sites/careerservicesforjdalumni" },
+                  { title: "CPC Career Services for J.D. Alumni", href: "/sites/careerservicesforjdalumni_old" },
+                  { title: "CPC Staff Workspace", href: "/sites/cpcworkspace" },
+                  { title: "DAUR Staff Workspace", href: "/sites/developmentworkspace" },
+                  { title: "Disability Services", href: "/sites/disabilityservices" },
+                  { title: "ELR Volume 34 Workspace (deactivated)", href: "/sites/elrvolume34" },
+                  { title: "ELR Volume 33 Workspace", href: "/sites/elrvolume33" },
+                  { title: "Event Planning", href: "/sites/eventplanning" },
+                  { title: "Exam Schedules", href: "/sites/examschedules" },
+                  { title: "Exam4 Registration, Proctoring, and Scheduling App", href: "/sites/erpsa" },
+                  { title: "Examinations", href: "/sites/examinations" },
+                  { title: "Externship Evaluations", href: "/sites/externshipevaluations" },
+                  { title: "Faculty Connect", href: "/sites/facultyconnect" },
+                  { title: "Faculty Support Workspace", href: "/sites/facultysupport" }
+                ]
+              },
+              {
+                title: "",
+                items: [
+                  { title: "Financial Aid Resources", href: "/sites/financialaidresources" },
+                  { title: "Financial Aid Staff Workspace", href: "/sites/financialaidworkspace" },
+                  { title: "Flex Time Manager", href: "/sites/comptimemanager" },
                   { title: "Forms Central", href: "/sites/formscentral" },
-                  { title: "Student Resources", href: "/sites/studentresources" }
+                  { title: "Full-time Faculty Workspace", href: "/sites/fulltimefacultyworkspace" },
+                  { title: "Fundamental Lawyering Skills", href: "/sites/fundamentallawyeringskills" },
+                  { title: "Grading", href: "/sites/grading" },
+                  { title: "Graduate Programs (LL.M., M.S.L., S.J.D.)", href: "/sites/nonjdstudentresources" },
+                  { title: "Help Desk Staff Workspace", href: "/sites/helpdeskworkspace" },
+                  { title: "House 1/2", href: "/sites/house1-2" },
+                  { title: "House 3/4", href: "/sites/house3-4" },
+                  { title: "House 5/6", href: "/sites/house5-6" },
+                  { title: "House 7/8", href: "/sites/house7-8" },
+                  { title: "House 9 (Evening Division)", href: "/sites/house-evedivision" },
+                  { title: "House 9/10 (deactivated)", href: "/sites/house9-10" },
+                  { title: "ILJ Volume 45 Workspace", href: "/sites/iljvolume45" }
+                ]
+              },
+              {
+                title: "",
+                items: [
+                  { title: "ILJ Volume 46 Workspace", href: "/sites/iljvolume46" },
+                  { title: "ILJ Volume 47 Workspace", href: "/sites/iljvolume47" },
+                  { title: "ILJ Volume 48 Workspace", href: "/sites/iljvolume48" },
+                  { title: "International Programs Staff Workspace", href: "/sites/internationalprogramsworkspace" },
+                  { title: "IPLJ Volume XXXII Workspace (deactivated)", href: "/sites/ipljvolumexxxii" },
+                  { title: "IPLJ Volume XXXIII Workspace (deactivated)", href: "/sites/ipljvolumexxxiii" },
+                  { title: "J.D. Admissions Staff Workspace", href: "/sites/jdadmissionsworkspace" },
+                  { title: "J.D. Concentrations", href: "/sites/jdconcentrations" },
+                  { title: "J.D. Curriculum Guide", href: "/sites/jdcurriculumguide" },
+                  { title: "J.D. Externships", href: "/sites/jdexternships" },
+                  { title: "J.D. Graduation Audit", href: "/sites/jdgraduationaudit" },
+                  { title: "JCFL Volume XXIX Workspace", href: "/sites/jcflvolumexxix" },
+                  { title: "JCFL Volume XXVII Workspace", href: "/sites/jcflvolumexxvii" },
+                  { title: "JCFL Volume XXVIII Workspace", href: "/sites/jcflvolumexxviii" }
+                ]
+              },
+              {
+                title: "",
+                items: [
+                  { title: "JCFL Volume XXX Workspace", href: "/sites/jcflvolumexxx" },
+                  { title: "Legal English Institute (deactivated)", href: "/sites/legalenglishinstitute" },
+                  { title: "Legal Writing and Lawyering Staff Workspace", href: "/sites/legalwritingworkspace" },
+                  { title: "Marcomm Staff Workspace", href: "/sites/marcomstaffworkspace" },
+                  { title: "Mental Health and Wellness Resources", href: "/sites/mentalhealthandwellness" },
+                  { title: "Named Scholarships Workspace", href: "/sites/namedscholarships" },
+                  { title: "Office of Finance Staff Workspace", href: "/sites/financeworkspace" },
+                  { title: "Office of Professionalism", href: "/sites/professionalism" },
+                  { title: "Office of Student Affairs Staff Workspace", href: "/sites/officeofstudentaffairsworkspace" },
+                  { title: "OPP Staff Workspace", href: "/sites/oppworkspace" },
+                  { title: "PIRC Staff Workspace", href: "/sites/pircstaffworkspace" },
+                  { title: "Pro Bono Scholars Program", href: "/sites/probonoscholarsprogram" },
+                  { title: "Professionalism Staff Workspace", href: "/sites/professionalismstaff" },
+                  { title: "Public Interest Resource Center", href: "/sites/publicinterestresourcecenter" },
+                  { title: "Public Interest Resource Center", href: "/sites/publicinterestresourcecenter_old" }
                 ]
               }
             ]
@@ -244,6 +334,62 @@ export class MegaMenuService {
         {
           title: "Library",
           href: "https://library.law.fordham.edu/"
+        },
+        {
+          title: "Apps",
+          href: "",
+          megaMenu: {
+            columns: [
+              {
+                title: "Global apps",
+                items: [
+                  { title: "25 Live", href: "https://25live.collegenet.com/25live/data/fordham/run/login.shibboleth?redirect=https://25live.collegenet.com/fordham/" },
+                  { title: "Blackboard", href: "https://fordham.blackboard.com/" },
+                  { title: "Canva", href: "/SitePages/Canva.aspx?web=1" },
+                  { title: "Echo360", href: "http://bit.ly/3pcUJWh" },
+                  { title: "Fordham U Apps", href: "https://www.fordham.edu/my-apps/" },
+                  { title: "GMail", href: "https://gmail.fordham.edu/" },
+                  { title: "Google Drive", href: "https://drive.google.com/a/fordham.edu" },
+                  { title: "Grammarly", href: "https://www.grammarly.com/enterprise/signup" },
+                  { title: "Microsoft Office 365", href: "https://login.microsoftonline.com/login.srf?wa=wsignin1.0&amp%3bwhr=fordham.edu" },
+                  { title: "Power BI", href: "https://app.powerbi.com/" },
+                  { title: "Qualtrics", href: "https://fordham.qualtrics.com/" },
+                  { title: "Seating Charts", href: "https://zdapi.appointlink.com/ZDConvert/AutoLaunch/2990/0/0/x7fg/login=shib%7cidp=https:%28s%29%28s%29loginp.fordham.edu%28s%29idp%28s%29shibboleth%7czdapp=zd" },
+                  { title: "TWEN", href: "https://lawschool.thomsonreuters.com/ls-login.php" },
+                  { title: "Web Print Portal", href: "https://print.fordham.edu/" }
+                ]
+              },
+              {
+                title: "Academic apps",
+                items: [
+                  { title: "Anonymous Course Evaluations", href: "/sites/courseevaluations" },
+                  { title: "Bookstore", href: "https://urldefense.proofpoint.com/v2/url?u=https-3A__sso.bncollege.com_bes-2Dsp_bessso_saml_fordhamedu_aip_logon&d=DwMF-g&c=aqMfXOEvEJQh2iQMCb7Wy8l0sPnURkcqADc2guUW8IM&r=0d_-pEHmOTsg1xcUtiEH_76ojRaVa2Ag-v8ew8GAJxg&m=dEBVCNKNONg5RrRMPc3Mf3Pqjti0eG_FTKB0cQaSnuw&s=19DiuVpjLa9ttsjIgm2eMN4-y94mLc2Q9Lle4Y6Q5cM&e=" },
+                  { title: "Class Schedules", href: "https://myweb.lawnet.fordham.edu/sites/classschedules" },
+                  { title: "Course Syllabi and First Assignments", href: "/SitePages/Course-Syllabi-and-First-Assignments.aspx" },
+                  { title: "Examinations (Exam4)", href: "/sites/examinations" }
+                ]
+              },
+              {
+                title: "Student apps",
+                items: [
+                  { title: "Faculty Connect", href: "/SitePages/Faculty-Connect.aspx" },
+                  { title: "Lockers", href: "/SitePages/Student-Lockers.aspx?web=1" },
+                  { title: "My Bill Suite", href: "http://bit.ly/2GB2wZw" },
+                  { title: "Student Registration", href: "https://reg-prod.ec.fordham.edu/StudentRegistrationSsb/ssb/registration" },
+                  { title: "Student Services", href: "https://studentssb-prod.ec.fordham.edu/StudentSelfService" }
+                ]
+              },
+              {
+                title: "Staff apps",
+                items: [
+                  { title: "Employee Services", href: "https://www.fordham.edu/my-pages/employee" },
+                  { title: "Clerkship Database", href: "/sites/clerkship/SitePages/Clerkship-Applicants.aspx" },
+                  { title: "Clerskship Interview Feedback", href: "/sites/cjecworkspace/SitePages/Interview-Feedback-Admin.aspx" },
+                  { title: "Course Tools", href: "/SitePages/Course-Tools.aspx" }
+                ]
+              }
+            ]
+          }
         },
         {
           title: "IT Support Portal",
